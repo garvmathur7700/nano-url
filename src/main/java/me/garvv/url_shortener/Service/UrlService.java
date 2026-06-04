e02e8e3f5eb331e63ac9029cbf5efafa8b64d990
Garv Mathur <Garv.Mathur@cognizant.com>
2026-06-04 20:17:05 +0530

    feat: add shorten service method logic

--- src/main/java/me/garvv/url_shortener/Service/UrlService.java
+++ src/main/java/me/garvv/url_shortener/Service/UrlService.java
@@ -1,6 +1,52 @@
 package me.garvv.url_shortener.Service;
 
+import jakarta.transaction.Transactional;
+import me.garvv.url_shortener.Model.Url;
+import me.garvv.url_shortener.Repository.UrlRepository;
+import me.garvv.url_shortener.Utils.Base62;
+import me.garvv.url_shortener.Utils.SecureRandomNumberGenerator;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
-public class UrlService {}
+public class UrlService {
+
+    private final UrlRepository urlRepository;
+
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @Transactional()
+    public String shortenUrl (String longUrl) {
+        // Generate shortUrl
+        String shortUrl = Base62.encode(SecureRandomNumberGenerator.getRandomLong());
+
+        // If shortUrl already exists in DB then handle collision
+        Optional<String> longUrlInRecord = urlRepository.findLongUrlByShortUrl(shortUrl);
+
+        // collision handling:
+        if (longUrlInRecord.isPresent()) {
+
+            // if shortUrl and longUrl match, return same shortUrl
+            if (longUrlInRecord.toString().equals(longUrl))
+                return shortUrl;
+
+                // if shortUrl matches but not longUrl, then re-encode
+            else
+                shortenUrl(longUrl);
+        }
+
+        // Else, save shortUrl, longUrl, createdAt to repository
+        Url url = new Url();
+        url.setLongUrl(longUrl);
+        url.setShortUrl(shortUrl);
+        url.setCreatedAt(LocalDateTime.now());
+
+        urlRepository.save(url);
+
+        return url.getShortUrl();
+    }
+}
