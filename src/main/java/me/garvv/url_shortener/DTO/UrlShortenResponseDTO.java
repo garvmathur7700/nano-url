dbf97a3966cda3843f87a0ec9cc524f08171f0fe
Garv Mathur <Garv.Mathur@cognizant.com>
2026-06-04 20:13:42 +0530

    feat: add DTO for shorten response

--- src/main/java/me/garvv/url_shortener/DTO/UrlShortenResponseDTO.java
+++ src/main/java/me/garvv/url_shortener/DTO/UrlShortenResponseDTO.java
@@ -1,5 +1,8 @@
 package me.garvv.url_shortener.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UrlShortenResponseDTO(
+        @NotBlank(message = "shortUrl is required")
         String shortUrl
 ) {}
