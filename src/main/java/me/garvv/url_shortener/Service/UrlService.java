adcb4255b44142148cee96f053502888482f1fa1
Garv Mathur <garvmathur7700@gmail.com>
2026-06-06 12:19:12 +0530

    feat: add redirection method in service interface

--- src/main/java/me/garvv/url_shortener/Service/UrlService.java
+++ src/main/java/me/garvv/url_shortener/Service/UrlService.java
@@ -1,8 +1,10 @@
 package me.garvv.url_shortener.Service;
 
+import me.garvv.url_shortener.DTO.UrlRedirectionResponseDTO;
 import me.garvv.url_shortener.DTO.UrlShortenResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface UrlService {
-    public ResponseEntity<UrlShortenResponseDTO> createNewShortUrl (String longUrl);
+    public UrlShortenResponseDTO createNewShortUrl (String longUrl);
+    public UrlRedirectionResponseDTO getLongUrl (String shortUrl);
 }
