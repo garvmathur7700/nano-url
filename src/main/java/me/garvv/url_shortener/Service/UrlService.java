6994c51f9103772d27b1ada795baaa0633b78289
Garv Mathur <garvmathur7700@gmail.com>
2026-06-06 10:27:11 +0530

    refactor: converted UrlService to interface and added an implementation class

--- src/main/java/me/garvv/url_shortener/Service/UrlService.java
+++ src/main/java/me/garvv/url_shortener/Service/UrlService.java
@@ -1,52 +1,8 @@
 package me.garvv.url_shortener.Service;
 
-import jakarta.transaction.Transactional;
-import me.garvv.url_shortener.Model.Url;
-import me.garvv.url_shortener.Repository.UrlRepository;
-import me.garvv.url_shortener.Utils.Base62;
-import me.garvv.url_shortener.Utils.SecureRandomNumberGenerator;
-import org.springframework.stereotype.Service;
+import me.garvv.url_shortener.DTO.UrlShortenResponseDTO;
+import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-@Service
-public class UrlService {
-
-    private final UrlRepository urlRepository;
-
-    public UrlService(UrlRepository urlRepository) {
-        this.urlRepository = urlRepository;
-    }
-
-    @Transactional
-    public String shortenUrl (String longUrl) {
-        // Generate shortUrl
-        String shortUrl = Base62.encode(SecureRandomNumberGenerator.getRandomLong());
-
-        // fetch longUrl from DB, `null` if not exists
-        Optional<String> longUrlInRecord = urlRepository.findLongUrlByShortUrl(shortUrl);
-
-        // collision handling:
-        if (longUrlInRecord.isPresent()) {
-
-            // if shortUrl and longUrl match, return same shortUrl
-            if (longUrlInRecord.equals(Optional.of(longUrl)))
-                return shortUrl;
-
-            // if shortUrl matches but not longUrl, then re-encode
-            else
-                shortenUrl(longUrl);
-        }
-
-        // Else, save shortUrl, longUrl, createdAt to repository
-        Url url = new Url();
-        url.setLongUrl(longUrl);
-        url.setShortUrl(shortUrl);
-        url.setCreatedAt(LocalDateTime.now());
-
-        urlRepository.save(url);
-
-        return url.getShortUrl();
-    }
+public interface UrlService {
+    public ResponseEntity<UrlShortenResponseDTO> createNewShortUrl (String longUrl);
 }
