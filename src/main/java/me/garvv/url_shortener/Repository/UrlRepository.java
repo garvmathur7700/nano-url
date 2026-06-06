0dc95e6fccd8c93cab9f41e891f6375d5758f31d
Garv Mathur <garvmathur7700@gmail.com>
2026-06-06 12:16:32 +0530

    feat: added method to find long url by short url

--- src/main/java/me/garvv/url_shortener/Repository/UrlRepository.java
+++ src/main/java/me/garvv/url_shortener/Repository/UrlRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,6 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
     @Query("SELECT u.shortUrl FROM Url u WHERE u.longUrl = :longUrl")
     String findShortUrlByLongUrl(@Param("longUrl") String longUrl);
     Boolean existsByLongUrl(String longUrl);
+    @Query("SELECT u.longUrl FROM Url u WHERE u.shortUrl = :shortUrl")
+    Optional<String> findLongUrlByShortUrl(@Param("shortUrl") String shortUrl);
 }
