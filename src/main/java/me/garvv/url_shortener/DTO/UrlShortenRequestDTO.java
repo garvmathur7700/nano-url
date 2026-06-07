8575c757fafe50391df1b4b0288f22078498b091
Garv Mathur <garvmathur7700@gmail.com>
2026-06-07 11:47:20 +0530

    fix: minor changes

--- src/main/java/me/garvv/url_shortener/DTO/UrlShortenRequestDTO.java
+++ src/main/java/me/garvv/url_shortener/DTO/UrlShortenRequestDTO.java
@@ -1,9 +1,11 @@
 package me.garvv.url_shortener.DTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record UrlShortenRequestDTO (
+        @NotEmpty(message = "longUrl is required")
         @NotBlank(message = "longUrl is required")
         String longUrl
 ){}
