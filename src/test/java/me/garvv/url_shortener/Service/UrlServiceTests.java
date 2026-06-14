e6af8876928502baddff103fe85f9c13111c8eca
Garv Mathur <garvmathur7700@gmail.com>
2026-06-14 15:08:49 +0530

    chore: update readme with jmeter test results

---
+++ src/test/java/me/garvv/url_shortener/Service/UrlServiceTests.java
@@ -0,0 +1,9 @@
+package me.garvv.url_shortener.Service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UrlServiceTests {
+
+}
