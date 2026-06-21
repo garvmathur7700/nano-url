c37b94ae36a6c8d092ff0ca2781098d4ac393a48
Garv Mathur <garvmathur7700@gmail.com>
2026-06-21 22:02:35 +0530

    tests: added mock mvc unit tests for createShortUrl controller method

--- src/test/java/me/garvv/url_shortener/Controller/UrlControllerTests.java
+++ src/test/java/me/garvv/url_shortener/Controller/UrlControllerTests.java
@@ -1,6 +1,8 @@
 package me.garvv.url_shortener.Controller;
 
 import me.garvv.url_shortener.DTO.UrlRedirectionResponseDTO;
+import me.garvv.url_shortener.DTO.UrlShortenRequestDTO;
+import me.garvv.url_shortener.DTO.UrlShortenResponseDTO;
 import me.garvv.url_shortener.Service.UrlServiceImpl;
 import me.garvv.url_shortener.exceptions.UrlNotFoundException;
 import org.junit.jupiter.api.Test;
@@ -9,14 +11,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = {UrlController.class})
 @AutoConfigureMockMvc(addFilters = false)
@@ -81,9 +84,31 @@ public class UrlControllerTests {
     /***********************************************************/
 
     /**
-     * POST `createShortUrl test start here
+     * POST `createShortUrl` tests start here
      */
 
     @Test
-    public void UrlController_createShortUrl_given
+    public void UrlController_createShortUrl_givenValidDto_shouldReturn201AndShortUrl() throws Exception {
+        // Arrange
+        String longUrl = "https://www.google.com";
+        String shortUrl = "Abc123";
+        UrlShortenRequestDTO requestDTO = new UrlShortenRequestDTO(longUrl);
+        UrlShortenResponseDTO responseDTO = new UrlShortenResponseDTO(shortUrl, longUrl);
+
+        when(urlService.createNewShortUrl(requestDTO.longUrl())).thenReturn(responseDTO);
+
+        // Act
+        mockMvc.perform(post("/api/url/shorten")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDTO)))
+
+        // Assert
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.shortUrl").value(shortUrl));
+    }
+
+    @Test
+    public void UrlController_createShortUrl_givenInvalidDto_shouldReturn400() throws Exception {
+
+    }
 }
